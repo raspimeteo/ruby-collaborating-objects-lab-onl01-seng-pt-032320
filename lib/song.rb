7ed536b0ab878179ebe1c1b0779445b31f6faf4c
John Zonneveld <jjczon@gmail.com>
2020-04-15 16:27:28 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -29,8 +29,8 @@ class Song
         song
     end
 
-    # def artist_name= (artist_name)
-    #     Artist.find_or_create_by_name(artist_name)
-    # end
+    def artist_name= (artist_name)
+        Artist.find_or_create_by_name(artist_name)
+    end
 
   end
